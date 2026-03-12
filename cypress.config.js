01f6934ba0412ded4642e0fb640c2964fbec754a
Darіa Zhaloba <d.zhaloba@gmail.com>
2026-03-12 20:18:50 +0200

    Initial Cypress setup

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://example.cypress.io",
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    video: false,
+    screenshotOnRunFailure: true,
+    retries: 1
+  }
+});
